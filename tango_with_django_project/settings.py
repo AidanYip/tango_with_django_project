80211cb7bbccaaa45ee27162b48a7ce699d3e031
“Aidan <2789553y@student.gla.ac.uk>
2024-09-13 01:17:18 +0100

    Ch 11 + Ex

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -103,8 +104,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# If True, users can register.
+REGISTRATION_OPEN = True
+
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'rango:index'
+
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login'
+
 # Login fail redirect
-LOGIN_URL = 'rango:login'
+# LOGIN_URL = 'rango:login'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
